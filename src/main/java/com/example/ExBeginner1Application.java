811b4af7937e7a52aab60845679465a31c13f80e
kanjiro-ryu <kanjiro.ryu@rakus-partners.co.jp>
2020-06-16 12:56:58 +0900

    コミットしました

---
+++ src/main/java/com/example/ExBeginner1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBeginner1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBeginner1Application.class, args);
+	}
+
+}
